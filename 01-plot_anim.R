c0ba5c53aa9e166e244f9ea823a50149b5ef7a3b
diegopenhanut <diegopenhanut@gmail.com>
2020-04-02 09:09:52 -0300

    Add ministério da saúde new data
    mudando fonte de dados para tabela que o ministério da saúde começou
     a fornecer em 01 de abril.

--- 01-plot_anim.R
+++ 01-plot_anim.R
@@ -13,25 +13,48 @@ library("magick")
 
 # Lendo arquivos com casos
 
-casos <- read_csv2("output/casos_compilados.csv")
+# casos <- read_csv2("output/casos_compilados.csv")
+
+covid <- read_csv2("dados/COVID19_20200401.csv") %>%
+	mutate(data = dmy(data)) %>%
+	filter(data >= ymd("2020-02-26"))
+
+mapa_sigla <-
+	structure(list(estado = c("AC", "AL", "AP", "AM", "BA", "CE",             
+							 "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE",        
+							 "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"),             
+				   nome = c("ACRE", "ALAGOAS", "AMAPÁ", "AMAZONAS", "BAHIA",            
+							"CEARÁ", "DISTRITO FEDERAL", "ESPÍRITO SANTO", "GOIÁS",              
+							"MARANHÃO", "MATO GROSSO", "MATO GROSSO DO SUL", "MINAS GERAIS",     
+							"PARÁ", "PARAÍBA", "PARANÁ", "PERNAMBUCO", "PIAUÍ", "RIO DE JANEIRO",
+							"RIO GRANDE DO NORTE", "RIO GRANDE DO SUL", "RONDÔNIA",              
+							"RORAIMA", "SANTA CATARINA", "SÃO PAULO", "SERGIPE", "TOCANTINS"     
+							)), row.names = c(NA, -27L), class = "data.frame")                   
+
+
+covid <- left_join(covid, mapa_sigla)
+
+covid 
 
 # Variáveis que serão usadas depois
 
-hoje <- format(Sys.time(), "%A, %d de %B de %Y")
+hoje <- Sys.Date()
 
-ultimo_dia <- max(casos$dia)
+ultimo_dia <- max(covid$data)
 
-primeiro_dia <- min(casos$dia)
+primeiro_dia <- ymd("2020-02-26")
 
 
 # gráfico de linha
 
-brasil <- casos %>%
-  group_by(dia) %>%
-  summarise(confirmados = sum(confirmados, na.rm = TRUE),
-            óbitos = sum(obitos, na.rm = TRUE)) %>%
-  gather(key = tipo, value = casos, -dia) %>%
-  mutate(tipo = factor(x = tipo, levels = c("óbitos", "confirmados")))
+brasil <- covid %>%
+  group_by(data) %>%
+  summarise(confirmados = sum(casosAcumulados, na.rm = TRUE),
+            obitos = sum(obitosAcumulado, na.rm = TRUE)) %>%
+  gather(key = tipo, value = casos, -data) %>%
+  mutate(tipo = factor(x = tipo, levels = c("obitos", "confirmados")))
+
+brasil
 
 brasil <- mutate(brasil, casos = if_else(casos == 0, as.numeric(NA), casos)) 
 
@@ -39,11 +62,12 @@ brasil <- mutate(brasil, casos = if_else(casos == 0, as.numeric(NA), casos))
 
 # sem animação, brasil
 
-simples_brasil <- ggplot(brasil, aes(x = dia, y = casos, color = tipo)) +
+simples_brasil <- ggplot(brasil, aes(x = data, y = casos, color = tipo)) +
   geom_line() +
   theme_minimal() +
   xlab("Dia") + 
-  ylab("Confirmados") 
+  ylab("Confirmados") +
+  xlim(c(primeiro_dia, ultimo_dia))
 
 simples_brasil
 
@@ -53,7 +77,7 @@ ggsave(filename = "plots/brasil_linear.png", simples_brasil, device = "png")
 # tentando extrapolar usando log para uma semana de casos
 
 predicao_brasil <- filter(brasil, tipo == "confirmados") %>%
-  ggplot(aes(x = dia, y = casos, color = tipo)) +
+  ggplot(aes(x = data, y = casos, color = tipo)) +
   scale_y_continuous(trans='log10') + 
   geom_point() +
   xlim(primeiro_dia, ultimo_dia + 7) +
@@ -70,16 +94,16 @@ ggsave(filename = "plots/brasil_predicao.png", predicao_brasil, device = "png")
 
 # gráfico de barras, com último dia
 
-estados <- casos %>%
-	filter(dia == ultimo_dia) %>%
-  select(sigla, confirmados, obitos) %>%
-  gather(key = 'tipo', value = 'casos', -sigla) %>%
-  mutate(tipo = factor(x = tipo, levels = c("obitos", "confirmados")))
+estados <- covid %>%
+	filter(data == ultimo_dia) %>%
+  select(estado, casosAcumulados, obitosAcumulado) %>%
+  gather(key = 'tipo', value = 'casos', -estado) %>%
+  mutate(tipo = factor(x = tipo, levels = c("obitosAcumulado", "casosAcumulados")))
 
 
 # sem animação, estados
 
-simples_estados <- ggplot(estados, aes(x = sigla, y = casos, fill = tipo)) +
+simples_estados <- ggplot(estados, aes(x = estado, y = casos, fill = tipo)) +
   geom_col(position = 'dodge') +
   ggtitle(paste0("Casos confirmados por estado em ", ultimo_dia, " COVID-19")) + 
   xlab("Estados") + 
@@ -93,10 +117,10 @@ ggsave(filename = "plots/estados_barra.png", simples_estados, device = "png")
 
 # com animação, brasil
 
-ani_brasil <- ggplot(brasil, aes(x = dia, y = casos, color = tipo)) +
+ani_brasil <- ggplot(brasil, aes(x = data, y = casos, color = tipo)) +
   geom_line() +
   geom_point(size = 2) + 
-  transition_reveal(dia) +
+  transition_reveal(data) +
   labs(title = 'Casos confirmados no Brasil - COVID-19') +
   theme_minimal() +
   xlab("Dia") + 
@@ -107,15 +131,15 @@ ani_brasil
 anim_save(filename = "animações/brasil_linear.gif")
 
 
-estados_anim <- casos %>%
-  select(sigla, confirmados, obitos, dia) %>%
-  gather(key = 'tipo', value = 'casos', -sigla, -dia) %>%
-  mutate(tipo = factor(x = tipo, levels = c("obitos", "confirmados")))
+estados_anim <- covid %>%
+  select(estado, casosAcumulados, obitosAcumulado, data) %>%
+  gather(key = 'tipo', value = 'casos', -estado, -data) %>%
+  mutate(tipo = factor(x = tipo, levels = c("obitosAcumulado", "casosAcumulados")))
 
 
-anim_estado_bar <- ggplot(estados_anim, aes(x = sigla, y = casos, fill = tipo)) +
+anim_estado_bar <- ggplot(estados_anim, aes(x = estado, y = casos, fill = tipo)) +
   geom_col(position = 'dodge') +
-  transition_time(dia) +
+  transition_time(data) +
   labs(title = 'Casos confirmados no Brasil - COVID-19',
      subtitle = 'dia {frame_time}') +
   theme_minimal() +
@@ -132,9 +156,9 @@ br <- get_brmap(geo = "State",
           class = "sf")
 
 anim_mapa <- 
-  plot_brmap(br, data_to_join = casos, 
+  plot_brmap(br, data_to_join = covid, 
              join_by = c("nome" = "nome"),
-             var = "confirmados") + 
+             var = "casosAcumulados") + 
   coord_sf(datum = NA) +
   labs(title = "Brasil - COVID-19", 
        subtitle = paste0(
@@ -143,7 +167,7 @@ anim_mapa <-
        )) +
   ylab("") +
   xlab("") +
-  transition_time(dia) +
+  transition_time(data) +
   scale_fill_viridis_c(na.value = 0, trans = 'log10', guide = "legend") +
   theme_minimal()
 
